2f9fb29db79dea287ab65f810610b3d9ff0db938
javierbenavides <javierhbr@gmail.com>
2025-08-28 00:54:22 -0500

    Updates

--- assets/circle-alert-DUfNsyZ5.js
+++ assets/circle-alert-DUfNsyZ5.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DeksGDDi.js";/**
+import{c as e}from"./index-CfEYXZBA.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/save-E0gEuOLB.js
+++ assets/save-E0gEuOLB.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DeksGDDi.js";/**
+import{c as a}from"./index-CfEYXZBA.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
